3cfa13342210bfabdf0b6d8b5b3e5328af0a1cdb
godshang <godshang@163.com>
2020-04-20 22:31:57 +0800

    update

---
+++ algorithms/java/src/main/java/Q258_Add_Digits/Solution.java
@@ -0,0 +1,19 @@
+package Q258_Add_Digits;
+
+public class Solution {
+
+    public int addDigits(int num) {
+        if (num < 10) return num;
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num = num / 10;
+        }
+        return addDigits(sum);
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.addDigits(38));
+    }
+}
