c6701baa1ce18fc89828e553dedcce267bda7add
godshang <godshang@163.com>
2020-04-21 22:29:32 +0800

    update

---
+++ algorithms/java/src/main/java/Q278_First_Bad_Version/Solution.java
@@ -0,0 +1,29 @@
+package Q278_First_Bad_Version;
+
+public class Solution extends VersionControl {
+
+    public int firstBadVersion(int n) {
+        int left = 1, right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.firstBadVersion(5));
+    }
+}
+
+class VersionControl {
+
+    boolean isBadVersion(int version) {
+        return version >= 4;
+    }
+}
