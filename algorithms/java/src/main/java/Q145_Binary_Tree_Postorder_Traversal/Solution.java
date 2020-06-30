0b83beefb39e4f9654ee56899162dd71ecb22db8
godshang <godshang@163.com>
2020-06-30 21:34:00 +0800

    update

---
+++ algorithms/java/src/main/java/Q145_Binary_Tree_Postorder_Traversal/Solution.java
@@ -0,0 +1,36 @@
+package Q145_Binary_Tree_Postorder_Traversal;
+
+import common.TreeNode;
+
+import java.util.*;
+
+public class Solution {
+
+    // 左->右->父
+    public List<Integer> postorderTraversal(TreeNode root) {
+        if (root == null) return Collections.emptyList();
+        List<Integer> resultList = new ArrayList<>();
+        Deque<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if (node != null) {
+                stack.push(node);
+                stack.push(null);
+                if (node.right != null) stack.push(node.right);
+                if (node.left != null) stack.push(node.left);
+            } else {
+                resultList.add(stack.pop().val);
+            }
+        }
+        return resultList;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+        System.out.println(solution.postorderTraversal(root));
+    }
+}
