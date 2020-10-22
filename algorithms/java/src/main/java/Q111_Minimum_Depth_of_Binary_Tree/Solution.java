4fab0d55c145d0ff119a51f5aac20a37d0eeaf72
godshang <godshang@163.com>
2020-10-22 23:06:00 +0800

    update

--- algorithms/java/src/main/java/Q111_Minimum_Depth_of_Binary_Tree/Solution.java
+++ algorithms/java/src/main/java/Q111_Minimum_Depth_of_Binary_Tree/Solution.java
@@ -2,9 +2,33 @@ package Q111_Minimum_Depth_of_Binary_Tree;
 
 import common.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Solution {
 
     public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int depth = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+                if (cur.left == null && cur.right == null)
+                    return depth;
+                if (cur.left != null)
+                    queue.offer(cur.left);
+                if (cur.right != null)
+                    queue.offer(cur.right);
+            }
+            depth++;
+        }
+        return depth;
+    }
+
+    public int minDepth_dfs(TreeNode root) {
         if (root == null) return 0;
         return helper(root, Integer.MAX_VALUE, 1);
     }
