c13cf4d0699d43f6df4e3f552594c0e27bcc34f6
godshang <godshang@163.com>
2020-11-15 18:51:44 +0800

    update

---
+++ algorithms/java/src/main/java/Q341_Flatten_Nested_List_Iterator/NestedInteger.java
@@ -0,0 +1,12 @@
+package Q341_Flatten_Nested_List_Iterator;
+
+import java.util.List;
+
+public interface NestedInteger {
+
+    boolean isInteger();
+
+    Integer getInteger();
+
+    List<NestedInteger> getList();
+}
