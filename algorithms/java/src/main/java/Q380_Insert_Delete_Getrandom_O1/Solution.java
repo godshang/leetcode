8cc6f1e37b3513dfeb5cc5f0ed03d7135fd2fbd5
godshang <godshang@163.com>
2020-12-05 19:37:28 +0800

    380

---
+++ algorithms/java/src/main/java/Q380_Insert_Delete_Getrandom_O1/Solution.java
@@ -0,0 +1,15 @@
+package Q380_Insert_Delete_Getrandom_O1;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        RandomizedSet randomizedSet = new RandomizedSet();
+        System.out.println(randomizedSet.insert(1));
+        System.out.println(randomizedSet.remove(2));
+        System.out.println(randomizedSet.insert(2));
+        System.out.println(randomizedSet.getRandom());
+        System.out.println(randomizedSet.remove(1));
+        System.out.println(randomizedSet.insert(2));
+        System.out.println(randomizedSet.getRandom());
+    }
+}
