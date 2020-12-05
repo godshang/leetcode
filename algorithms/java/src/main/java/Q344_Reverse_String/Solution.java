e2a12e37df49e2b9c4c434ebda1d9820b9263b18
godshang <godshang@163.com>
2020-12-05 17:42:08 +0800

    344

---
+++ algorithms/java/src/main/java/Q344_Reverse_String/Solution.java
@@ -0,0 +1,23 @@
+package Q344_Reverse_String;
+
+public class Solution {
+
+    public void reverseString(char[] s) {
+        if (s == null) return;
+        int left = 0, right = s.length - 1;
+        while (left < right) {
+            char tmp = s[left];
+            s[left] = s[right];
+            s[right] = tmp;
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        char[] s = {'h','e','l','l','o'};
+        solution.reverseString(s);
+        System.out.println(new String(s));
+    }
+}
