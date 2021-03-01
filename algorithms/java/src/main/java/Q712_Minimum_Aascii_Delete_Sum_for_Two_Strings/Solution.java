4d71effabc451e7fa9501fcd7764b5c664f4d459
godshang <godshang@163.com>
2021-03-01 22:30:47 +0800

    update

---
+++ algorithms/java/src/main/java/Q712_Minimum_Aascii_Delete_Sum_for_Two_Strings/Solution.java
@@ -0,0 +1,16 @@
+package Q712_Minimum_Aascii_Delete_Sum_for_Two_Strings;
+
+public class Solution {
+
+    public int minimumDeleteSum(String s1, String s2) {
+        
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        Character A = 'A', B= 'B';
+        System.out.println(A.hashCode());
+        System.out.println(B.hashCode());
+    }
+}
