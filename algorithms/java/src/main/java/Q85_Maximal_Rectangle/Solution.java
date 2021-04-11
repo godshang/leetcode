b2950c687d9d5fac765e91ba8daf23fdd8bff3e8
godshang <godshang@163.com>
2021-04-11 23:01:15 +0800

    update

---
+++ algorithms/java/src/main/java/Q85_Maximal_Rectangle/Solution.java
@@ -0,0 +1,12 @@
+package Q85_Maximal_Rectangle;
+
+public class Solution {
+
+    public int maximalRectangle(char[][] matrix) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+    }
+}
