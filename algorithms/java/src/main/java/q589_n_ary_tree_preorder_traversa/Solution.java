275b7de1c5d99f8a77ec86129f238f414f03d5f1
godshang <godshang@163.com>
2022-03-10 21:36:09 +0800

    update

---
+++ algorithms/java/src/main/java/q589_n_ary_tree_preorder_traversa/Solution.java
@@ -0,0 +1,45 @@
+package q589_n_ary_tree_preorder_traversa;
+
+import common.Node;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Solution {
+
+    public List<Integer> preorder(Node root) {
+        List<Integer> ans = new ArrayList<>();
+        if (root == null) return ans;
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node node = stack.pop();
+            if (node != null) {
+                List<Node> children = node.children;
+                if (children != null) {
+                    for (int i = children.size() - 1; i >= 0; i--) {
+                        stack.push(node.children.get(i));
+                    }
+                }
+                stack.push(node);
+                stack.push(null);
+            } else {
+                ans.add(stack.pop().val);
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        Node node = new Node(1, new ArrayList<>());
+        node.children.add(new Node(3, new ArrayList<>()));
+        node.children.add(new Node(2));
+        node.children.add(new Node(4));
+        node.children.get(0).children.add(new Node(5));
+        node.children.get(0).children.add(new Node(6));
+        System.out.println(solution.preorder(node));
+    }
+}
